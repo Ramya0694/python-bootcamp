163c11a31b65a541736ea12593a682729563791a
Ramya0694 <ramya0694@gmail.com>
2019-08-03 15:28:53 -0700

    myFile.py

--- myFile.py
+++ myFile.py
@@ -1,2 +1,44 @@
-print("This is my file")
-print("Hello")
+x=15
+y=3
+z = "This"
+
+#Concatenate strings
+print(z*y)
+
+#Automated spacing
+print(x,y,z)
+
+#For loop
+for i in range(0,10):
+    print(i)
+
+#List
+myList = [10,20,30,40]
+
+for i in myList:
+    print(i.index())
+
+#Function
+def myPrint(words):
+    print(words, ", ok.")
+
+myPrint("are you")
+
+def recFun(vList):
+    if len(vList) > 0:
+        print(vList.pop())
+        recFun(vList)
+
+recFun([1,"star",3.14, "Hi Hi"])
+
+#While
+while x > 0:
+    print(x)
+    x = x - 1
+
+def myRecFun(vList,index,step):
+    if index < len(vList):
+        print(vList[index])
+        myRecFun(vList,index + step,step)
+    
+myRecFun([1,2,3,4],0,2)
